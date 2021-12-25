e87bbc02220c06e346c5007e018e04583b8564e9
Simon Hryszko <simon@hryszko.dev>
2021-12-25 06:36:02 +0100

    Write first version of script

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# autoScreenshotter
-Linux script what will make screenshot of your e-learning class
+# screenShotter
+
+ScreenShotter is script what will make screenshots of your desktop and hold only different. It will keep all of them, but only different will pack to .tar file.
+
+## Directories
+Script will create directories:
+- Downloads
+  - $(date)
+	  - screenshots - for holding all make screenshots
+		- logs - for holding information about difference between screenshots
+		  There file names will be contain useful information about difference between screenshots (nameFromScreenshots:ifSame:pixelsDifference:percentDifference:timeDifference)
+		- tar - to put only different files
+
+## Installation
+
+Clone this repo, script will download additional application if needed
+```sh
+git clone https://github.com/Shirobachi/screenShotter.git
+```
+
+## Usage
+### Run 
+Run `screenShotter.sh $1 $2` file
+$1 and $2 are optional where:
+- $1 - time to wait between screenshots
+- $2 - minimal difference between screenshots
+
+### close
+Remove PID file from `/tmp/screenShotter.sh`
+
+! Simple killing script will not make tar file, so please follow above method
+
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+If is any feature what you would like to be added, you open issue :)
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ screenShotter.sh
@@ -0,0 +1,147 @@
+#!/bin/bash
+
+set -x
+
+# get script filename without extension
+SCRIPT_NAME=$(basename "$0" | cut -d. -f1)
+
+# check if PID file exists
+if [ -f /tmp/"$SCRIPT_NAME".pid ]; then
+	echo "PID file exists"
+	exit 1
+else
+	# save PID 
+	echo $$ > /tmp/"$SCRIPT_NAME".pid
+fi
+
+# remove PID and /tmp $SCRIPT_NAME.png on exit
+trap 'rm -f /tmp/$SCRIPT_NAME.pid /tmp/$SCRIPT_NAME.png /tmp/$SCRIPT_NAME.compare.png' EXIT
+
+# if --help or -h is given, print help
+if [ "$1" == "--help" ] || [ "$1" == "-h" ]; then
+	echo "Usage:"
+	echo "$0 -h, --help: Print this help"
+	echo "$0 -x: debug mode on"
+	echo "$0 3: Set delay to 2 seconds"
+	echo "$0 3 0.15: Set delay to 2 seconds and set minimal difference to 15%"
+	echo
+	echo Thank you for using screemshotter.sh by @shirobachi
+	exit 0
+fi
+
+# set -x if flag is set
+if [ "$1" = "-x" ]; then
+	set -x
+else
+	# check if is 1 argument
+	if [ "$1" = "" ]; then
+		sleeper=1
+	else
+		sleeper=$1
+	fi
+
+	# check if is 2 empty
+	if [ "$2" = "" ]; then
+		border="0.11"
+	else
+		border="$2"
+	fi
+fi
+
+# let show notifcation from bg
+export DISPLAY=:0.0
+
+# check if magick installed
+if [ ! "$(command -v magick)" ]; then
+	# Get place to save magick
+	path=$(echo "$PATH" | tr ":" "\n" | head -1)
+	
+	# Download macick from url
+	link="https://download.imagemagick.org/ImageMagick/download/binaries/magick"
+	notify-send "Downloading magick" "Please wait..."
+	wget "$link" -O "$path"/magick
+	notify-send "Downloading magick" "Done"
+	chmod +x "$path"/magick
+fi
+
+# --------------------------------------------------
+
+## make variable with current date
+date=$(date +%Y-%m-%d_%H-%M-%S)
+
+mkdir -p "$HOME/Downloads/$date/logs"
+mkdir -p "$HOME/Downloads/$date/screenshots"
+
+# wait 5 seconds
+sleep 5
+
+while true; do
+
+	# kill notifcation during screenshot
+	killall -q dunst
+
+	# name of made ss
+	ssFilename=$(date +%Y-%m-%d_%H-%M-%S-%3N)
+	ssPath="$HOME/Downloads/$date/screenshots/$ssFilename.png"
+
+	# make full screen screenshot
+	magick import -window root "$ssPath"
+	sleep .5
+
+	# check if have ss to compare
+	if [ -f "/tmp/$SCRIPT_NAME.png" ]; then
+		# compare ss
+		diff=$(magick compare -metric RMSE -subimage-search "$ssPath" "/tmp/$SCRIPT_NAME.png" "/tmp/$SCRIPT_NAME.compare.png" 2>&1)
+
+		par1=$(echo "$diff" | cut -d' ' -f1)
+		par2=$(echo "$diff" | cut -d'(' -f2 | cut -d')' -f1)
+		par3=$(echo "$diff" | cut -d' ' -f4)
+
+		# if diff is more than 0.11
+		if [ "$(echo "$par2 > $border" | bc)" -eq 1 ]; then
+			same=true
+		else
+			same=false
+		fi
+
+		# cp and overwrite ss
+		cp "$ssPath" "/tmp/$SCRIPT_NAME.png"
+
+		mv "/tmp/$SCRIPT_NAME.compare.png" "$HOME/Downloads/$date/logs/$ssFilename:$same:$par1:$par2:$par3.png"
+	else # if it's very 1st ss
+		# mv file to compare destination
+		cp "$ssPath" "/tmp/$SCRIPT_NAME.png"
+	fi
+
+	# check if pid file exists
+	if [ ! -f /tmp/"$SCRIPT_NAME".pid ]; then
+		# check if pid is still running
+
+		break 
+	fi
+
+	sleep "$sleeper"
+done
+
+# make dir for different screenshots
+mkdir "$HOME/Downloads/$date/tar"
+
+# mv the very first ss
+# find the first ss
+latest=$(find "$HOME/Downloads/$date/screenshots" -type f -printf "%T@ %p\n" | sort -n | cut -d' ' -f2- | head -1)
+
+cp "$latest" "$HOME/Downloads/$date/tar"
+
+# find all logs with true
+loops=$(find "$HOME/Downloads/$date/logs" -type f -name "*true*")
+for i in $loops; do
+	file=$(basename "$i" | cut -d':' -f1).png
+	cp "$HOME/Downloads/$date/screenshots/$file" "$HOME/Downloads/$date/tar"
+
+	# cp "$i" "$HOME/Downloads/$date/tar"
+done
+
+# make tar
+cd "$HOME/Downloads/$date/tar" || exit
+tar -czf "$HOME/Downloads/$date/$date.tar.gz" ./*
+cd - || exit
